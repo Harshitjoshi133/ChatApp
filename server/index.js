1f43513ddd015119a1df7a10da6c3545715bc981
Harshitjoshi133 <116452069+Harshitjoshi133@users.noreply.github.com>
2024-07-15 14:43:58 +0530

    Backend Pointing

--- server/index.js
+++ server/index.js
@@ -1,51 +1,54 @@
-const express=require('express');
-const cors=require("cors");
-const mongoose=require("mongoose");
-const socket=require("socket.io");
-const userRoutes=require("./routes/userRoute")
-const messageRoutes=require("./routes/messageRoutes")
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const socket = require('socket.io');
+const userRoutes = require('./routes/userRoute');
+const messageRoutes = require('./routes/messageRoutes');
+require('dotenv').config();
 
-const app=express();
+const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://chat-app-tan-six-70.vercel.app', // Update with your frontend origin
+    credentials: true
+}));
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/auth",userRoutes);
-app.use("/api/messages",messageRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/api/messages', messageRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("Express Vercel App");
-})
+app.get('/', (req, res) => {
+    res.send('Express Vercel App');
+});
 
-const mongoURI=process.env.MONGO_URL;
-mongoose.connect(mongoURI, { })
+const mongoURI = process.env.MONGO_URL;
+mongoose.connect(mongoURI, {})
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Listening on PORT ${process.env.PORT}`);
+});
 
+const io = socket(server, {
+    cors: {
+        origin: 'https://chat-app-tan-six-70.vercel.app', // Update with your frontend origin
+        credentials: true
+    }
+});
 
-const server = app.listen(process.env.PORT,()=>{
-    console.log(`Listening on PORT ${process.env.PORT}`)
-})
+global.onlineUsers = new Map();
 
-const io=socket(server,{
-    cors:"http://localhost:5173",
-    credentials:true,
-})
-
-global.onlineUsers=new Map();
-
-io.on("connection",(socket)=>{
-    global.chatSocket=socket;
-    socket.on("add-users",(userId)=>{
-        onlineUsers.set(userId,socket.id);
-    })
-    socket.on("send-msg",(data)=>{
-        const sendUserSocket=onlineUsers.get(data.to);
-        if(sendUserSocket){
-            socket.to(sendUserSocket).emit("msg-recieve",data.message);
+io.on('connection', (socket) => {
+    global.chatSocket = socket;
+    socket.on('add-users', (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+    socket.on('send-msg', (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('msg-recieve', data.message);
         }
-    })
-})
+    });
+});
