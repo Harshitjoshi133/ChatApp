caf3bcf8dc25a7d6e76e55ce5d2355bc03f39d58
Harshitjoshi133 <116452069+Harshitjoshi133@users.noreply.github.com>
2024-07-16 12:03:54 +0530

    Too Much

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -8,57 +7,54 @@ const messageRoutes = require('./routes/messageRoutes');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5500;
+const PORT=process.env.PORT || 5500;
 
-// CORS options
+
 const corsOptions = {
-  origin: 'https://chat-app-tan-six-70.vercel.app', // Your frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 200,
+   origin: '*',
+   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+   credentials: true,
+   optionsSuccessStatus: 200,
 };
 
-// Enable CORS
 app.use(cors(corsOptions));
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', userRoutes);
 app.use('/api/messages', messageRoutes);
 
-// MongoDB connection
+app.get('/', (req, res) => {
+    res.send("GET Request Called")
+})
 const mongoURI = process.env.MONGO_URL;
 mongoose.connect(mongoURI, {})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
-// Start server
 const server = app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}`);
+    console.log(`Listening on PORT ${PORT}`);
 });
 
-// Socket.io setup
 const io = socket(server, {
-  cors: {
-    origin: 'https://chat-app-tan-six-70.vercel.app', // Your frontend URL
-    credentials: true,
-    optionSuccessStatus: 200,
-  },
+    cors: {
+        origin: '*', // Update with your frontend origin
+        credentials: true,
+        optionSuccessStatus:200,
+    }
 });
 
 global.onlineUsers = new Map();
 
 io.on('connection', (socket) => {
-  global.chatSocket = socket;
-  socket.on('add-users', (userId) => {
-    onlineUsers.set(userId, socket.id);
-  });
-  socket.on('send-msg', (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit('msg-recieve', data.message);
-    }
-  });
+    global.chatSocket = socket;
+    socket.on('add-users', (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+    socket.on('send-msg', (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('msg-recieve', data.message);
+        }
+    });
 });
