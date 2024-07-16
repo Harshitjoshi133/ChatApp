ed6e03787f8e426fad9d9a4bb7da36287f42ef8d
Harshitjoshi133 <116452069+Harshitjoshi133@users.noreply.github.com>
2024-07-16 21:04:50 +0530

    Sockets Debugging

--- server/index.js
+++ server/index.js
@@ -37,6 +37,7 @@ const io = socket(server, {
     cors: {
         origin: 'https://chat-app-tan-six-70.vercel.app', // Update with your frontend origin
         credentials: true,
+        methods:[GET,POST],
         transports: ['websocket', 'polling'],
         allowedHeaders: ['Content-Type'],
         optionSuccessStatus:200,
